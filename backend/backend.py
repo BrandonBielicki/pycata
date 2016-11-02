1f82de44c726eb7ee0643d70c25c7108b57644e8
Brandon Bielicki <Brandonbielicki@gmail.com>
2016-11-02 01:40:01 +0000

    Connected to main feed, pulling and pushing to firebase

--- backend/backend.py
+++ backend/backend.py
@@ -1,11 +1,34 @@
 #import gtfs_realtime_pb2
 from google.transit import gtfs_realtime_pb2
 import urllib
+import auth
+import requests
 
+auth_key=auth.auth_key
+ftp_url=auth.ftp_url
+fb_timestamp_url="https://sizzling-fire-5776.firebaseio.com/timestamp.json?auth="+auth_key
+feed_url="http://developers.cata.org/gtfsrt/GTFS-RealTime/TrapezeRealTimeFeed.pb"
+
+def firebaseCall(_url, _method, _data):
+  if(_method == "post"):
+    response = requests.post(_url, _data)
+  elif(_method == "get"):
+    response = requests.get(_url, _data)
+  elif(_method == "put"):
+    response = requests.put(_url, _data)
+  elif(_method == "delete"):
+    response = requests.delete(_url, _data)
+  content = response.content
+  code = response.status_code
+  return response
+  
 feed = gtfs_realtime_pb2.FeedMessage()
-response = urllib.urlopen('http://developers.cata.org/gtfsrt/vehicle/vehiclepositions.pb')
+response = urllib.urlopen(feed_url)
 feed.ParseFromString(response.read())
+firebaseCall(fb_timestamp_url,"put",str(feed.header.timestamp));
 print(feed.header.timestamp)
+
+
 for entity in feed.entity:
   if entity.HasField('trip_update'):
     print (entity.trip_update)
