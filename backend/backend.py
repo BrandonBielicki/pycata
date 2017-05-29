938ca2dabf6ec30bb3d099849559b38124f02761
Brandon Bielicki <brandonbielicki@gmail.com>
2017-05-29 09:14:34 -0400

    added getRoutes function

--- backend/backend.py
+++ backend/backend.py
@@ -62,7 +62,8 @@ def firebaseCall(_url, _method, _data):
     content = response.content
     code = response.status_code
     return response
-  except:
+  except Exception, e:
+    print("firebaseCall - Error: " + str(e))
     return False
   
 def updateTimeStamp():
@@ -71,14 +72,16 @@ def updateTimeStamp():
     feed.ParseFromString(urllib2.urlopen(vehicle_feed_url).read())
     firebaseCall(fb_timestamp_url,"put",str(feed.header.timestamp))
     return True
-  except:
+  except Exception, e:
+    print("updateTimeStamp - Error: " + str(e))
     return False
 
 def deleteTrips():
   try:
     firebaseCall(fb_trip_url,"delete","")
     return True
-  except:
+  except Exception, e:
+    print("deleteTrips - Error: " + str(e))
     return False
   
 def updateTrips():
@@ -118,17 +121,19 @@ def updateTrips():
     firebaseCall(fb_trip_url,"put",json.dumps(data))
     return True
   except Exception, e:
-    print("Error in updateTrips - Error: " + str(e))
+    print("updateTrips - Error: " + str(e))
     return False
 
 def deleteStops():
   try:
     firebaseCall(fb_stop_url,"delete","")
     return True
-  except:
+  except Exception, e:
+    print("deleteStops - Error: " + str(e))
     return False
 
 def getRouteStops(route, gtfs):
+  try:
     stops = gtfs.executeQuery("""SELECT DISTINCT id, code, name, latitude, longitude
     FROM stops S
     INNER JOIN stop_times T
@@ -137,111 +142,15 @@ def getRouteStops(route, gtfs):
         ON Tr.trip_id = T.trip_id
     WHERE Tr.route_id = %s""", (route,))
     return stops
-
-#unused
-def updateStops():
-  stops_db = MySQLdb.connect(host=db_host, user=db_user, passwd=db_pass, db=db_db)
-  stops_conn = stops_db.cursor()
-  try:
-    feed = gtfs_realtime_pb2.FeedMessage()
-    feed.ParseFromString(urllib2.urlopen(trip_feed_url).read())
-    stops={}
-    
-    ################## QUESTIONABLE ##################
-    json_data = json.loads(firebaseCall(fb_stop_url,"get","").content)
-    if(json_data is not None):
-      for key, value in json_data.iteritems():
-        _route_num = key
-        if(_route_num not in stops):
-          stops[_route_num]=[]
-        for key, value in value.iteritems():
-          stops[_route_num].append(str(key))
-    ################## QUESTIONABLE ##################
-    
-    for entity in feed.entity:
-      _route_id=entity.trip_update.trip.route_id
-      if(_route_id == 261):
-        _route_id = 26
-      if(_route_id not in stops):
-        stops[_route_id]=[]
-      for stop in entity.trip_update.stop_time_update:
-        stop_id = stop.stop_id
-        stops[_route_id].append(str(stop_id))
-    
-      for key, value in stops.iteritems():
-        stop_names = set(value)
-        _route_id = key
-        for item in stop_names:
-          try:
-            _lat=0
-            _long=0
-            stops_conn.execute("SELECT id, code, latitude, longitude FROM stops WHERE code='"+ item +"'")
-            row = stops_conn.fetchone()
-            if row is not None:
-              _id = row[0]
-              _code = row[1]
-              _lat=row[2]
-              _long=row[3]
-              update_str ="{ \"id\": \""+str(_id)+"\", \"code\": \""+str(_code)+"\", \"latitude\": \""+str(_lat)+"\", \"longitude\": \""+str(_long)+"\"}"
-              firebaseCall(fb_base_url+"/stops/"+str(_route_id)+"/"+item+".json?auth="+auth_key,"patch",update_str)
-          except:
-            #print("    Error in " + item + " route " + _route_id )
-            pass
-    return True
-  except:
-    print("    Error in updateStops")
+  except Exception, e:
+    print("getRouteStops - Error: " + str(e))
     return False
 
-#unused
-def updateStopTimes(bottom, top):
-  def getTripsFromRoute(feed, route):
-    trips=[]
-    for entity in feed.entity:
-      _trip_id=entity.id
-      _route_id=entity.trip_update.trip.route_id
-      if(str(_route_id) == "261"):
-        _route_id ="26"
-      if(_route_id == route):
-        trips.append(_trip_id)
-    return trips
-     
-  try:
-    time_db = MySQLdb.connect(host=db_host, user=db_user, passwd=db_pass, db=db_db)
-    time_conn = time_db.cursor()
-    feed = gtfs_realtime_pb2.FeedMessage()
-    feed.ParseFromString(urllib2.urlopen(trip_feed_url).read())
-    json_data = json.loads(firebaseCall(fb_stop_url,"get","").content)
-    if(json_data is not None):
-      for key, value in json_data.iteritems():
-        if(int(key) > bottom and int(key) <= top):
-          _route_num = key
-          trips = getTripsFromRoute(feed,_route_num)
-          cur_time = str(datetime.datetime.strftime(datetime.datetime.now(), '%H:%M:%S'))
-          if(len(trips) > 0):
-            trip_id=str(trips[0])
-            for key, value in value.iteritems():
-                _stop_code = key
-                stop_id = str(value['id'])
-                sql_stmt="select arrival from trips t, stop_times s where service_id=(select service_id from trips where trip_id="+trip_id+") and t.trip_id=s.trip_id and t.route_id="+_route_num+" and s.stop_id="+stop_id+" and arrival>='"+cur_time+"' order by arrival limit 3"
-                time_conn.execute(sql_stmt)
-                time1="No Time Available"
-                time2="No Time Available"
-                time3="No Time Available"
-                try:
-                  time = time_conn.fetchone()[0]
-                  time1 = datetime.datetime.strptime(str(time), "%H:%M:%S").strftime("%I:%M")
-                  time = time_conn.fetchone()[0]
-                  time2 = datetime.datetime.strptime(str(time), "%H:%M:%S").strftime("%I:%M")
-                  time = time_conn.fetchone()[0]
-                  time3 = datetime.datetime.strptime(str(time), "%H:%M:%S").strftime("%I:%M")
-                except:
-                  pass
-                update_str = "{ \"1\" : \""+str(time1)+"\",\"2\" : \""+str(time2)+"\",\"3\" : \""+str(time3)+"\"}"
-                firebaseCall(fb_base_url+"/stops/"+str(_route_num)+"/"+str(_stop_code)+".json?auth="+auth_key,"patch",update_str) 
-    return True
-  except:
-    print("Error in updateStopTimes")
-    return False
+def getRoutes(gtfs):
+  routes = gtfs.executeQuery("""SELECT id, name
+  FROM routes
+  """)
+  return routes
 
 #db = MySQLdb.connect(host=db_host, user=db_user, passwd=db_pass, db=db_db)
 #conn = db.cursor()
@@ -257,6 +166,5 @@ while(True):
     timer = getCurrentTime()
     
   feed_timestamp = waitForUpdate(feed_timestamp) 
-  print("Starting main loop at " + str(feed_timestamp))
   updateTimeStamp()
   updateTrips()
